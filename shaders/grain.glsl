8cb6af46351ecd6759d9a45f2b2632ed8ef063f5
Otec Fura <fura.otec@gmail.com>
2015-03-28 09:05:47 +0100

    indentation sahder

--- shaders/grain.glsl
+++ shaders/grain.glsl
@@ -1,13 +1,13 @@
-		extern number opacity;
-		extern number grainsize;
-		extern number noise;
-		extern Image noisetex;
-		extern vec2 tex_ratio;
-		float rand(vec2 co)
-		{
-			return Texel(noisetex, mod(co * tex_ratio / vec2(grainsize), vec2(1.0))).r;
-		}
-		vec4 effect(vec4 color, Image texture, vec2 tc, vec2 _)
-		{
-			return color * Texel(texture, tc) * mix(1.0, rand(tc+vec2(noise)), opacity);
-		}
+extern number opacity;
+extern number grainsize;
+extern number noise;
+extern Image noisetex;
+extern vec2 tex_ratio;
+
+float rand(vec2 co){
+	return Texel(noisetex, mod(co * tex_ratio / vec2(grainsize), vec2(1.0))).r;
+}
+
+vec4 effect(vec4 color, Image texture, vec2 tc, vec2 _){
+	return color * Texel(texture, tc) * mix(1.0, rand(tc+vec2(noise)), opacity);
+}
